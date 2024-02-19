de81b8deb6c0295d846b54edc3ebd95b8401aac0
dennisl843 <159915842+dennisl843@users.noreply.github.com>
2024-02-19 14:29:15 -0700

    Add files via upload

---
+++ Module 1 Critical Thinking.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print('Enter a whole number:', end=' ')
+num1 = int(input())
+print('Enter another whole number:', end=' ')
+num2 = int(input())
+added = num1 + num2
+subtracted = num1 - num2
+print('The sum of these numbers is', added)
+print('The difference of these numbers is', subtracted)
+#%%
+print('Enter a whole number:', end=' ')
+num1 = int(input())
+print('Enter another whole number:', end=' ')
+num2 = int(input())
+multiplied = num1*num2
+divided = num1/num2
+print('The product of these numbers is', multiplied)
+print('The dividend of these numbers is', divided)
